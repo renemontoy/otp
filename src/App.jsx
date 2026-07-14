0e7d5176fdb2ca35463a25ec5f52002d67dce7b2
Rene Montoy <rivan@pxg.com>
2026-07-14 15:52:29 -0700

    Odontograma

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Pacientes from "./pages/Pacientes";
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import Agenda from "./pages/Agenda";
+
 function App() {
     return (
         <div className="app">
--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,49 +1,36 @@
-import StatsCards from "../components/StatsCards/StatsCards";
-import PatientTable from "../components/PatientTable/PatientTable";
-import PatientProfile from "../components/PatientProfile/PatientProfile";
-import Odontogram from "../components/Odontogram/Odontogram";
-import History from "../components/History/History";
-import UpcomingAppointments from "../components/UpcomingAppointments/UpcomingAppointments";
-import { useState, useEffect} from "react";    
-import {getPatients} from "../supabase/patients";
-
+import { Odontogram } from "react-odontogram";
+import "react-odontogram/style.css";
 
 function Agenda() {
 
-    const [patients, setPatients] = useState([]);
-    const [selectedPatient, setSelectedPatient] = useState(null);
-    useEffect(() => {
-
-        async function loadPatients() {
-
-            try {
-
-                const data = await getPatients();
-
-                if (!data || data.length === 0) return;
-
-                setPatients(data);
-                setSelectedPatient(data[0]);
-
-            } catch (error) {
-
-                console.error(error);
-
-            }
-
+  const handleChange = (selectedTeeth) => {
+    console.log(selectedTeeth);
+    /*
+      Example output:
+      [
+        {
+          "id": "teeth-21",
+          "notations": {
+            "fdi": "21",
+            "universal": "9",
+            "palmer": "1UL"
+          },
+          "type": "Central Incisor"
+        },
+        {
+          "id": "teeth-12",
+          "notations": {
+            "fdi": "12",
+            "universal": "7",
+            "palmer": "2UR"
+          },
+          "type": "Lateral Incisor"
         }
+      ]
+    */
+  };
 
-        loadPatients();
-
-    }, []);
-
-    return (
-        <div className="dashboard">
-
-            <h2 className="dashboardTitle">Trabajando en la agenda...</h2>
-
-        </div>
-    );
+  return <Odontogram onChange={handleChange} />;
 }
 
 export default Agenda;
